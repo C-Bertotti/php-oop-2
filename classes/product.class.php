a445ba18a778ebce9cb8b4efb6fa1fd0332d00ea
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-12 16:40:31 +0200

    Prodotto prova

--- classes/product.class.php
+++ classes/product.class.php
@@ -16,8 +16,51 @@ class Prodotto {
         $this->descrizione = $_descrizione;
         $this->codice = $_codice;
     }
-
 }
 
+$prodottoProva = new Prodotto(12.50, 'Padella', 'Ottima per omelette', 'AB123');
+var_dump($prodottoProva);
 
+/*
+class Prodotto {
+    //attributi
+    protected $titolo;
+    protected $autore;
+    protected $isbn;
+    protected $prezzo;
+
+    //costruttore
+
+    public function __construct($_titolo, $_autore, $_isbn, $_prezzo)
+    {
+        $this->titolo = $_titolo;
+        $this->autore = $_autore;
+        $this->isbn = $_isbn;
+        $this->prezzo = $_prezzo;
+    }
+
+    //metodi
+    public function getTitolo()
+    {
+        return $this->titolo;
+    }
+}
+
+class Manuale extends Libro {
+    protected $argomento;
+
+    //costruttore
+    public function __construct($_titolo, $_autore, $_isbn, $_prezzo, $_argomento)
+    {
+        // $this->titolo = $_titolo;
+        // $this->autore = $_autore;
+        // $this->isbn = $_isbn;
+        // $this->prezzo = $_prezzo;
+
+        parent:: __construct($_titolo, $_autore, $_isbn, $_prezzo);
+        $this->argomento = $_argomento;
+    }
+}
+
+*/
 ?>
